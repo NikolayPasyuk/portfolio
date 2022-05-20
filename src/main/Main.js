d2645496b0c626f96a99547f5edabbd6f361b833
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-20 21:53:06 +0300

    added styles for pages Main

--- src/main/Main.js
+++ src/main/Main.js
@@ -1,16 +1,25 @@
 import style from './Main.module.css'
 import styleContainer from '../common/styles/Container.module.css'
+import sliderImage from "../assets/image/slider-bg.png";
+import photoImage from '../assets/image/photo.jpg'
 
 export const Main = () => {
+    const slider = {
+        backgroundImage: `url(${sliderImage})`
+    };
+    const photo = {
+        backgroundImage: `url(${photoImage})`
+    }
+
     return (
-        <div className={style.mainBlock}>
+        <div style={slider} className={style.mainBlock}>
             <div className={styleContainer.container}>
-                <div className={style.text}>
+                <div style={photo} className={style.photo}>
+                </div>
+                <div className={style.wrapper}>
                     <span>Hi there</span>
                     <h1>I am Nikolay Pasyuk</h1>
-                    <p>FRONT-END DEVELOPER</p>
-                </div>
-                <div className={style.photo}>
+                    <p className={style.visible}>FRONT-END DEVELOPER</p>
                 </div>
             </div>
         </div>
