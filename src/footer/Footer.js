ef066ba8f703bab0b5b19c88c6524cc0f981867d
NikolayPasyuk <kolyan-1812@mail.ru>
2022-05-21 20:45:31 +0300

    added new styles

--- src/footer/Footer.js
+++ src/footer/Footer.js
@@ -1,17 +1,18 @@
 import style from './Footer.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faLinkedin, faSkype, faTelegram, faTwitter} from '@fortawesome/free-brands-svg-icons'
+import {faLinkedin, faSkype, faTelegram} from '@fortawesome/free-brands-svg-icons'
+import {faEnvelope} from '@fortawesome/free-solid-svg-icons'
 
 export const Footer = () => {
     return (
         <div className={style.footerBlock}>
             <div className={`${styleContainer.container} ${style.footerContainer}`}>
                 <div>
-                    <FontAwesomeIcon className={style.icon} icon={faSkype}/>
-                    <FontAwesomeIcon className={style.icon} icon={faTwitter}/>
+                    <FontAwesomeIcon className={style.icon} icon={faEnvelope}/>
                     <FontAwesomeIcon className={style.icon} icon={faTelegram}/>
                     <FontAwesomeIcon className={style.icon} icon={faLinkedin}/>
+                    <FontAwesomeIcon className={style.icon} icon={faSkype}/>
                 </div>
                 <p>All rights reserved 2022</p>
             </div>
--- src/main/Main.js
+++ src/main/Main.js
@@ -1,4 +1,4 @@
-import style from './Main.module.css'
+import style from './Main.module.scss'
 import styleContainer from '../common/styles/Container.module.css'
 import sliderImage from "../assets/image/slider-bg.png";
 import photoImage from '../assets/image/photo.jpg'
@@ -17,9 +17,9 @@ export const Main = () => {
                 <div style={photo} className={style.photo}>
                 </div>
                 <div className={style.wrapper}>
-                    <span>Hi there</span>
-                    <h1>I am Nikolay Pasyuk</h1>
-                    <p className={style.visible}>FRONT-END DEVELOPER</p>
+                    <span>Hi THERE!</span>
+                    <h1>I'm Nikolay Pasyuk</h1>
+                    <p>FRONT-END DEVELOPER</p>
                 </div>
             </div>
         </div>
