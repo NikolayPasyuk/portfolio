6826e459142ecdceb6c3ec93e1f38371665515d8
NikolayPasyuk <kolyan-1812@mail.ru>
2022-08-02 20:23:03 +0300

    refactoring

--- src/projects/Projects.js
+++ src/projects/Projects.js
@@ -23,7 +23,7 @@ export const Projects = () => {
                 <Title text={'Projects'}/>
                 <div className={style.projects}>
                     <Project style={cards} title={'Playing cards'}
-                             href={'https://github.com/NikolayPasyuk/playing-cards'}
+                             href='https://github.com/NikolayPasyuk/playing-cards'
                              description={'SPA for learning by cards. Implemented: login page with validation; register page with validation; reset password page with validation; 404 page; profile page with abilities to: edit your profile information, add a new card pack and edit, search, sort, view, remove and learn your card packs; packs list page with abilities to: search, sort, view and learn any card packs; learn page with abilities to answer a question and evaluate your answer.'}/>
                     <Project style={social} title={'Social Network'}
                              href={'https://github.com/NikolayPasyuk/social-network'}
